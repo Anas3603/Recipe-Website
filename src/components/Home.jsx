36b1d3e6ed54ffba265cd17017d1a0771a23b645
Anas3603 <mohammadanasnisar@gmail.com>
2025-08-10 23:53:57 +0530

    React recipe website

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar"
+import PopularSlider from "./PopularSlider"
+import TrendingSlider from "./TrendingSlider"
+
+const Home = () => {
+  return (
+    <>
+      <div className="main">
+        <Navbar />
+        <PopularSlider />
+        <TrendingSlider />
+      </div>
+    </>
+  )
+}
+
+export default Home
